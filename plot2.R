df59cb97a665aa20b715a5f7aed605c82e6fecfc
Cristel <CristelVeefkind@users.noreply.github.com>
2016-06-13 09:42:31 +0200

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,17 @@
+#Plot 2
+
+#read the data as table make ? values into NA
+df_house <- read.table ("household_power_consumption.txt", header = TRUE, sep= ";",na.strings= c("?"))
+# I had issues with the as.date function so decided because of the time to read is as character
+# and subset it accordingly
+df_house$Date <- as.character(df_house$Date)
+df_house_2 <- subset(df_house, Date == "1/2/2007"| Date =="2/2/2007")
+df_house_2$DateTime <- paste(df_house_2$Date,df_house_2$Time, sep= " ")
+df_house_2$Date <- as.Date(df_house_2$Date, format = "%d/%m/%Y")
+df_house_2$DateTime <- strptime(df_house_2$DateTime,format= "%Y-%m-%d %H:%M:%S")
+
+#create the histogramg and save it to plot2.png
+png(file = "plot2.png", bg = "transparent", width= 768, height= 768)
+plot(df_house_2$DateTime,df_house_2$Global_active_power,type = "l",ylab="Global Active Power(kilowatts)",xlab = "")
+dev.off()
+
